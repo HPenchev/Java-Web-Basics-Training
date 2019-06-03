3d096cfb113b296cd9719bd0552527af1c77e95e
Hristo Penchev <hristo.penchev@paddypowerbetfair.com>
2019-06-03 19:58:25 +0300

    work

--- Java EE Servlet API 4.0/src/main/java/chushka/service/ProductServiceImpl.java
+++ Java EE Servlet API 4.0/src/main/java/chushka/service/ProductServiceImpl.java
@@ -1,16 +1,26 @@
 package chushka.service;
 
+import chushka.domain.entities.Product;
+import chushka.domain.entities.Type;
 import chushka.models.ProductServiceModels;
 import chushka.repository.ProductRepository;
+import chushka.util.ModelMapper;
 
 import javax.inject.Inject;
+import java.awt.*;
 
 public class ProductServiceImpl implements ProductService {
     @Inject
     private ProductRepository productRepository;
 
+    @Inject
+    private ModelMapper modelMapper;
+
     @Override
     public void saveProduct(ProductServiceModels productServiceModels) {
+        Product product = modelMapper.map(productServiceModels, Product.class);
+        product.setType(Type.valueOf(productServiceModels.getType()));
+        productRepository.save(product);
 
     }
 }
---
+++ Java EE Servlet API 4.0/src/main/java/chushka/web/servlets/IndexServlet.java
@@ -0,0 +1,8 @@
+package chushka.web.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+}
