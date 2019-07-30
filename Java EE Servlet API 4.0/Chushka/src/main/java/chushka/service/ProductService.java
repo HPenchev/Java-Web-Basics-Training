bac62b32a7130b18e5bff4cd19ed1cda813094fe
Hristo Penchev <hristo_penchev_86@abv.bg>
2019-07-30 21:53:31 +0300

    updates

---
+++ Java EE Servlet API 4.0/Chushka/src/main/java/chushka/service/ProductService.java
@@ -0,0 +1,6 @@
+package chushka.service;
+
+import chushka.models.ProductServiceModels;
+public interface ProductService {
+    void saveProduct(ProductServiceModels productServiceModels);
+}
--- Java EE Servlet API 4.0/src/main/java/chushka/web/servlets/IndexServlet.java
+++
@@ -1,8 +0,0 @@
-package chushka.web.servlets;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/")
-public class IndexServlet extends HttpServlet {
-}
